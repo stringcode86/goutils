370ce397959f5dbef6c13d78a1ab26f0032e759b
stringcode <michael@stringcode.co.uk>
2019-07-15 08:03:51 +0100

    Implementation of preprocesing’s pctChange & scale

--- math.go
+++ math.go
@@ -10,9 +10,16 @@ func AbsInt(x int) int {
 
 // PCTChange computes percent change
 func PCTChange(before float64, after float64) float64 {
-	diff := after - before
-	realDiff := diff / before
-	return realDiff
+	return after / before - 1
+}
+
+// Scales `val` using `mean` and `std` of training dataset. When scaling data 
+// via `preprocessing.Scale` `mean` and `std` are taken from entire data set.
+// When using model for prediciton `mean` and `std` woudl be computed only 
+// from given sample. This could result in inacurate predictions. This functions 
+// allows to pass mean and std of entire dataset 
+func Scale(val, mean, std float64) float64 {
+	return (val - mean) / std
 }
 
 // Max greater of two values
