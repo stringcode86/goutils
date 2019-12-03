0e343d8753e7f51868025cd011d4777d19af721c
stringcode <michael@stringcode.co.uk>
2019-12-03 08:23:59 +0000

    adding type conveninece methos

--- typeUtils.go
+++ typeUtils.go
@@ -15,3 +15,35 @@ func intfArrToFloat32Arr(intfArr []interface{}) []float32 {
 	}
 	return floats
 }
+
+func intfArrToIntArr(intfArr []interface{}) []int {
+	ints := make([]int, 0, len(intfArr))
+	for idx, val := range intfArr {
+		ints[idx] = val.(int)
+	}
+	return ints
+}
+
+func intfArrToInt64Arr(intfArr []interface{}) []int64 {
+	ints := make([]int64, 0, len(intfArr))
+	for idx, val := range intfArr {
+		ints[idx] = val.(int64)
+	}
+	return ints
+}
+
+func intfArrToUint64Arr(intfArr []interface{}) []uint64 {
+	ints := make([]uint64, 0, len(intfArr))
+	for idx, val := range intfArr {
+		ints[idx] = val.(uint64)
+	}
+	return ints
+}
+
+func intArrToFloat64Arr(intArr []int) []float64 {
+	floats := make([]float64, 0, len(intArr))
+	for idx, val := range intArr {
+		floats[idx] = float64(val)
+	}
+	return floats
+}
