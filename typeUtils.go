89cfd34f45490f80da37a067166bae237e15ed84
stringcode <michael@stringcode.co.uk>
2019-12-04 07:26:17 +0000

    fixing out of bounds crash

--- typeUtils.go
+++ typeUtils.go
@@ -1,7 +1,7 @@
 package goutils
 
 func IntfArrToFloat64Arr(intfArr []interface{}) []float64 {
-	floats := make([]float64, 0, len(intfArr))
+	floats := make([]float64, len(intfArr), len(intfArr))
 	for idx, val := range intfArr {
 		floats[idx] = val.(float64)
 	}
@@ -9,7 +9,7 @@ func IntfArrToFloat64Arr(intfArr []interface{}) []float64 {
 }
 
 func IntfArrToFloat32Arr(intfArr []interface{}) []float32 {
-	floats := make([]float32, 0, len(intfArr))
+	floats := make([]float32, len(intfArr), len(intfArr))
 	for idx, val := range intfArr {
 		floats[idx] = val.(float32)
 	}
@@ -17,7 +17,7 @@ func IntfArrToFloat32Arr(intfArr []interface{}) []float32 {
 }
 
 func IntfArrToIntArr(intfArr []interface{}) []int {
-	ints := make([]int, 0, len(intfArr))
+	ints := make([]int, len(intfArr), len(intfArr))
 	for idx, val := range intfArr {
 		ints[idx] = val.(int)
 	}
@@ -25,7 +25,7 @@ func IntfArrToIntArr(intfArr []interface{}) []int {
 }
 
 func IntfArrToInt64Arr(intfArr []interface{}) []int64 {
-	ints := make([]int64, 0, len(intfArr))
+	ints := make([]int64, len(intfArr), len(intfArr))
 	for idx, val := range intfArr {
 		ints[idx] = val.(int64)
 	}
@@ -33,7 +33,7 @@ func IntfArrToInt64Arr(intfArr []interface{}) []int64 {
 }
 
 func IntfArrToUint64Arr(intfArr []interface{}) []uint64 {
-	ints := make([]uint64, 0, len(intfArr))
+	ints := make([]uint64, len(intfArr), len(intfArr))
 	for idx, val := range intfArr {
 		ints[idx] = val.(uint64)
 	}
@@ -41,7 +41,7 @@ func IntfArrToUint64Arr(intfArr []interface{}) []uint64 {
 }
 
 func IntArrToFloat64Arr(intArr []int) []float64 {
-	floats := make([]float64, 0, len(intArr))
+	floats := make([]float64, len(intArr), len(intArr))
 	for idx, val := range intArr {
 		floats[idx] = float64(val)
 	}
